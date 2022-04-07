3d7092c2d29eb4e46141ddb9b761d750c494e4a5
Frank <fsolleveld@outlook.com>
2022-04-07 10:24:22 +0200

    Initial Commit

---
+++ Shared/NavigationatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationatorApp.swift
+//  Shared
+//
+//  Created by Frank Solleveld on 07/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct NavigationatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
